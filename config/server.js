e0292fed37061eab3c2ac94d8987114e6058e4ca
lechinoix <nicolas.ngomai@gmail.com>
2021-09-24 01:52:21 +0200

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '96fff9a873f6e086e8e203dd44bbf527'),
