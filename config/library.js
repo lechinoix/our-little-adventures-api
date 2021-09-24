6f14a1087a1a30c873ec0fdcfac7d4f6f096d0ea
lechinoix <nicolas.ngomai@gmail.com>
2021-09-25 01:16:34 +0200

    feat: update model to mathc needs

---
+++ config/library.js
@@ -0,0 +1,4 @@
+module.exports = ({ env }) => ({
+  GOOGLE_LIBRARY_CLIENT_ID: '424373709-5joqt8uoilp8ggrle69uef3jbbhornd5.apps.googleusercontent.com',
+  GOOGLE_LIBRARY_CLIENT_SECRET: '0NiwYn3lIjirAuOWBKNSiN7O'
+});
