5b50921aef8f8cffb12ebad014e7f3390515d2b9
lechinoix <nicolas.ngomai@gmail.com>
2021-11-08 03:12:04 +0100

    fix: build application in postbuild

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,6 +1,6 @@
 TMP_DIR=$(pwd)
 
-for file in $(grep -rl $TMP_DIR ${TMP_DIR}/vendor/vips/lib/pkgconfig)
+for file in $(grep -rl ${TMP_DIR}/vendor ${TMP_DIR}/vendor/vips/lib/pkgconfig)
 do
   sed -i "s+${TMP_DIR}/vendor+/app/vendor+g" $file
 done
