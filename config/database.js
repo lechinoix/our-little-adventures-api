44bca64952111167a34c23c4acab6bfee88e67d5
lechinoix <nicolas.ngomai@gmail.com>
2021-12-27 22:49:16 +0100

    feat: use postgres locally

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
-module.exports = ({ env }) => ({
+module.exports = () => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: '0.0.0.0',
+        port: 54320,
+        database: 'postgres',
+        username: 'postgres',
+        password: 'my_password',
       },
       options: {
-        useNullAsDefault: true,
+        autoMigration: true,
       },
     },
   },
